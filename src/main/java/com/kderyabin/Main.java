72a0f4eee4023b929b8c7d9526ebd8c9816785a6
Konstantin Deryabin <kderyabin@orange.fr>
2020-02-18 09:35:34 +0100

    docs and fxml tests

--- src/main/java/com/kderyabin/Main.java
+++ src/main/java/com/kderyabin/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
@@ -13,9 +14,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         //FXMLLoader loader = new FXMLLoader();
         //loader.setLocation(getClass().getResource("views/main.fxml"));
-        Parent root = FXMLLoader.load(Main.class.getResource("main.fxml")); //
-
+        Parent root = FXMLLoader.load(Main.class.getResource("main.fxml"));
         Scene scene = new Scene(root);
+        Font.loadFont(
+                this.getClass().getResource("assets/fonts/Roboto-Light.ttf").toExternalForm(), 24
+        );
+        scene.getStylesheets().add(
+                this.getClass().getResource("assets/style.css").toExternalForm()
+        );
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(scene);
         primaryStage.show();
---
+++ src/main/java/com/kderyabin/controllers/HeaderController.java
@@ -0,0 +1,9 @@
+package com.kderyabin.controllers;
+
+/**
+ * @author Konstantin Deryabin
+ * @date 17/02/2020
+ */
+
+public class HeaderController {
+}
