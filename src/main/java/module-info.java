8db5444e49227692aef4c754928ce517c5b0d359
Konstantin Deryabin <kderyabin@orange.fr>
2020-03-22 17:21:06 +0100

    migration on java 1.8

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.kderyabin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.kderyabin to javafx.fxml;
-    opens com.kderyabin.controllers to javafx.fxml;
-    exports com.kderyabin;
-}
