3d96c97535e4f45ebcfadbec4f603340f7d11619
Konstantin Deryabin <kderyabin@orange.fr>
2020-04-12 18:02:35 +0200

    updated menu actions

--- src/main/java/com/kderyabin/viewmodels/MenuViewModel.java
+++ src/main/java/com/kderyabin/viewmodels/MenuViewModel.java
@@ -1,12 +1,30 @@
 package com.kderyabin.viewmodels;
 
+import com.kderyabin.services.NavigateServiceInterface;
 import de.saxsys.mvvmfx.ViewModel;
 import javafx.application.Platform;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MenuViewModel implements ViewModel {
+
+    NavigateServiceInterface navigation;
+
+    public NavigateServiceInterface getNavigation() {
+        return navigation;
+    }
+
+    @Autowired
+    public void setNavigation(NavigateServiceInterface navigation) {
+        this.navigation = navigation;
+    }
+
     public void exit() {
         Platform.exit();
+    }
+
+    public void createNewBoard() throws Exception {
+        navigation.navigate("board-form");
     }
 }
--- src/main/java/com/kderyabin/views/MenuView.java
+++ src/main/java/com/kderyabin/views/MenuView.java
@@ -3,21 +3,21 @@ package com.kderyabin.views;
 import com.kderyabin.viewmodels.MenuViewModel;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
-import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
+import javafx.event.ActionEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MenuView implements FxmlView<MenuViewModel> {
 
-    @FXML
-    public MenuItem exitAppButton;
-
     @InjectViewModel
     private MenuViewModel viewModel;
 
-    public void initialize() {
-        exitAppButton.setOnAction( e -> viewModel.exit());
+
+    public void createNewBoard(ActionEvent actionEvent) throws Exception {
+        viewModel.createNewBoard();
     }
 
+    public void exit(ActionEvent actionEvent) {
+        viewModel.exit();
+    }
 }
